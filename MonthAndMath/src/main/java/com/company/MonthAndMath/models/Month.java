68872771f78a7ad117c3cf8e389ce1785d49ed2a
Roman <alonzofroman@gmail.com>
2022-05-09 20:19:13 -0700

    Added month class

--- MonthAndMath/src/main/java/com/company/MonthAndMath/models/Month.java
+++ MonthAndMath/src/main/java/com/company/MonthAndMath/models/Month.java
@@ -1,6 +1,42 @@
 package com.company.MonthAndMath.models;
 
+import java.util.Objects;
+
 public class Month {
     private String name;
     private Integer number;
+
+    public Month(String name, Integer number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(name, month.name) && Objects.equals(number, month.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, number);
+    }
 }
